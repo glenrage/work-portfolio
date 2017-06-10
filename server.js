a0cf0c989b3cbcda7cd20ccef044e713c97c5e16
galang pham <phamgalang@gmail.com>
2017-06-10 12:34:18 -0700

    deploy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000
 
 app.use (express.static('./public'));
 
-app.get('*', (req, res) => res.sendFile('public/index.html', {root: '.'}));
+app.get('/*', (req, res) => res.sendFile('public/index.html', {root: '.'}));
 
 app.listen(PORT, function(){
   console.log(`The PORT is: http://localhost:${PORT}`);
