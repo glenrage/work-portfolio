c87fc7947f4f916265551aef92b030e2f620a7f8
galang pham <phamgalang@gmail.com>
2017-06-10 13:43:31 -0700

    added overlay portfolio effects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,3 +6,14 @@ $(document).ready(function(){
     },6000);
   });
 });
+
+$("[rel='tooltip']").tooltip();
+
+$('.thumbnail').hover(
+    function(){
+        $(this).find('.caption').slideDown(250); //.fadeIn(250)
+    },
+    function(){
+        $(this).find('.caption').slideUp(250); //.fadeOut(205)
+    }
+); 
