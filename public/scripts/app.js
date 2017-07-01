b9139c2281cd7a8b688d10067dd202022e312b2f
galang pham <phamgalang@gmail.com>
2017-06-30 22:37:53 -0700

    added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,44 @@
 
-$(document).ready(function(){
-  $('.skillbar').each(function(){
-    $(this).find('.skillbar-bar').animate({
-      width:$(this).attr('data-percent')
-    },6000);
-  });
-});
+// $("[rel='tooltip']").tooltip();
+//
+// $('.thumbnail').hover(
+//     function(){
+//         $(this).find('.caption').slideDown(250); //.fadeIn(250)
+//     },
+//     function(){
+//         $(this).find('.caption').slideUp(250); //.fadeOut(205)
+//     }
+// );
 
-$("[rel='tooltip']").tooltip();
 
-$('.thumbnail').hover(
-    function(){
-        $(this).find('.caption').slideDown(250); //.fadeIn(250)
-    },
-    function(){
-        $(this).find('.caption').slideUp(250); //.fadeOut(205)
-    }
-); 
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+})(jQuery); // End of use strict
