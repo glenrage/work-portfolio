2b0aa2a814ffaedb0f7f197b773824cbf2baf40e
glenrage <phamgalang@gmail.com>
2018-05-15 17:22:22 -0700

    updated npm

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-'use strict';
-
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use (express.static('./public'));
+app.use(express.static('./public'));
 
-app.get('/*', (req, res) => res.sendFile('public/index.html', {root: '.'}));
+app.get('/*', (req, res) => res.sendFile('public/index.html', { root: '.' }));
 
-app.listen(PORT, function(){
+app.listen(PORT, function() {
   console.log(`The PORT is: http://localhost:${PORT}`);
 });
